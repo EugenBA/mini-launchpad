75d973d4fae71a61d592e0aec5cb000526b5ddb0
EugenB <b-eugen@yandex.ru>
2026-03-16 17:04:50 +0000

    change config frontend

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,6 +1,6 @@
 // Program IDs одинаковы для localnet и devnet (ключи из target/deploy)
-export const ORACLE_PROGRAM_ID = "4cuvLFFqhaKnTHfeq2FtTUvgudRSe7wq982fA9PBUqBU";
-export const MINTER_PROGRAM_ID = "E5erGzaxgCwHqH7RjLXLGWziXj8CXpyN7zW6BRodfFnE";
+export const ORACLE_PROGRAM_ID = "GyJHW5SbAXzUUPXNCUjgq9zJJnNqjj6kLECX1i3ynUWn";
+export const MINTER_PROGRAM_ID = "BvFGTCj3NFHrw54QMMHMnoxzvKXXUbgvEAR3LWj6jcuw";
 
 // Сети и RPC (переключатель на фронте)
 export type NetworkId = "localnet" | "devnet";
